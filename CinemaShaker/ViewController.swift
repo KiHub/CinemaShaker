33250e608e28d53fe738e6e11e334a3a6f5adefb
Kirill Ki <kirillkalapov@gmail.com>
2021-12-03 10:53:12 +0100

    Initial Commit

---
+++ CinemaShaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaShaker
+//
+//  Created by  Mr.Ki on 03.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
